40419d0ca8580d7831764df5abd0a853a49cc2c0
o-gkmn <ozgur.gokmen735@gmail.com>
2023-09-08 14:35:07 +0300

    who we arre partial was defined

--- RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultWhoWeAreComponentPartial.cs
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultWhoWeAreComponentPartial.cs
@@ -16,17 +16,23 @@ namespace RealEstate_Dapper_UI.ViewComponents.HomePage
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var client = _httpClientFactory.CreateClient();
+            var clientForServices = _httpClientFactory.CreateClient();
             var responseMessaage = await client.GetAsync("https://localhost:44382/api/WhoWeAre/who_we_ares");
+            var responseMessaageForServices = await clientForServices.GetAsync("https://localhost:44382/api/Services/get_service_list");
 
-            if(responseMessaage.IsSuccessStatusCode)
+            if(responseMessaage.IsSuccessStatusCode && responseMessaageForServices.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessaage.Content.ReadAsStringAsync();
+                var jsonDataForServices = await responseMessaageForServices.Content.ReadAsStringAsync();
+
                 var value = JsonConvert.DeserializeObject<List<ResultWhoWeAreDto>>(jsonData).FirstOrDefault();
+                var valueForServices = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonDataForServices);
+
                 ViewBag.title = value.title;
                 ViewBag.subtitle = value.subtitle;
                 ViewBag.description1 = value.description1;
                 ViewBag.description2 = value.description2;
-                return View();
+                return View(valueForServices);
             }
             return View();
         }
